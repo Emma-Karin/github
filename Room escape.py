e139b2658172694bd3fddc6c2ffdb9148bbcc9e7
Emma-Karin <emmakarin.rehnman@gmail.com>
2018-09-28 14:40:08 +0200

    Mitt första spel
    Mitt första spel

---
+++ Room escape.py
@@ -0,0 +1,29 @@
+#intro
+print("Knarr...")
+print("Gnissel...")
+print("...")
+print("...")
+print("...")
+print("BAAAM!")
+print("Dörren gick igen bakom dig - du som bara skulle kika in i det här övergivna gamla huset.")
+print("(Så här i efterhand var det kanske inte en jättebra idé.)")
+print("Du befinner dig i en mörk hall. Sikten är ungefär en och en halv meter framåt, och du kan se en smal trappa framför dig. Till vänster om den går en mörk, smal korridor längre in i huset.")
+print("Du utvärderar dina val och kommer fram till att du antingen kan försöka öppna dörren, gå upp för trappan eller undersöka korridoren.")
+
+current_position = 1
+
+#första valet
+ut_eller_in = input("Vad tänker du göra? ")
+
+#händelser för första valet
+if "
+
+if "trapp" or "höger" in ut_eller_in
+
+
+
+    
+        
+
+
+      
